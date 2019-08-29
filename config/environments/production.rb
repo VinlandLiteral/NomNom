97c2ea5d6e19917621dba99a1427ecf4478eb71b
Pablo Ortiz <paotiz91@gmail.com>
2019-08-29 04:09:59 +0000

    added devise gem which lets us use user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.defaut_url_options = {host: nomnom-pablo-ortiz.herokuapp.com}
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
