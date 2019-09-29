c409108bbbae3e83493553da96c52f17e2b13daa
Pablo Ortiz <paotiz91@gmail.com>
2019-09-29 03:04:00 +0000

    added email options

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomnomapp.com"
 
   def comment_added
-    mail(to: "paomaps00@gmail.com",
+    mail(to: "nomnom.1st.999@gmail.com",
           subject: "A comment has been added to your place")
   end
 end
