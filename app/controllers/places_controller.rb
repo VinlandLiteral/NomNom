cb3aba50d8cedb638aed155b0492f932f990a810
Pablo Ortiz <paotiz91@gmail.com>
2019-10-12 02:34:35 +0000

    fixed my index page for photo and fixed the show page for photo

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -51,9 +51,11 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
+    end
+
+    
     @place.destroy
     redirect_to root_path
-    end
   end
 
 
